b281608315b0fbe10dfe4546f21a569bafd8740b
ehossard <etiennehossard@gmail.com>
2022-11-22 11:26:04 +0100

    essaie calculette

---
+++ script.js
@@ -0,0 +1,37 @@
+let sum ='+'
+let sub ='-'
+let mult ='*'
+let div ='/'
+
+var a
+var b
+var c
+
+function sum(a,b) {
+    c=a + b
+}
+
+function sub(a,b){
+    c=a - b
+}
+
+function mult(a,b){
+    c=a * b
+}
+
+function div(a,b){
+    c=a / b
+}
+
+
+
+
+if (sign === '+') {
+    resultatOperation = a + b
+} else if (sign === '-') {
+    resultatOperation = a - b
+} else if (sign === '*') {
+    resultatOperation = a * b
+} else if (sign === '/') {
+    resultatOperation = a / b
+}
